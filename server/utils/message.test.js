5084c3cf4ae7b5935f0f0aeab893eff97cd11a25
wooojek <yokomura123@gmail.com>
2018-02-18 19:12:23 +0100

    Added geolocation support via geoloaction api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,27 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
         const from = 'Mike';
         const text = 'Elo';
-        const result = generateMessage(from, text);
+        const message = generateMessage(from, text);
 
-        expect(typeof result.createdAt).toEqual('number');
-        expect(result).toMatchObject({from, text});
+        expect(typeof message.createdAt).toEqual('number');
+        expect(message).toMatchObject({from, text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        const from = 'Mike';
+        const lat = 1;
+        const lng = 1;
+        const url = `https://www.google.com/maps?q=${lat},${lng}`;
+        const message = generateLocationMessage(from, lat, lng);
+
+        expect(typeof message.createdAt).toEqual('number');
+        expect(message).toMatchObject({from, url});
     });
 });
