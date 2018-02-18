576476471284d9952514e45ea227af9a6d7da503
wooojek <yokomura123@gmail.com>
2018-02-18 20:41:53 +0100

    removed console logs

--- server/server.js
+++ server/server.js
@@ -15,14 +15,12 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
 
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback();
     });
@@ -32,7 +30,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        io.emit('newMessage', generateMessage('Admin', 'User left'));
     })
 });
 
