bb28604181b24e7eb2cd08ff80857e19ee1500e1
wooojek <yokomura123@gmail.com>
2018-02-18 14:57:30 +0100

    init

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
