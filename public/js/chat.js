301ceebb116ce2ebbc942a895f9a2ced394a0243
wooojek <yokomura123@gmail.com>
2018-02-20 15:12:52 +0100

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -74,7 +74,6 @@ jQuery('#message-form').on('submit', function (event) {
     var messageTextBox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextBox.val()
     }, function () {
         messageTextBox.val('');
--- server/server.js
+++ server/server.js
@@ -38,12 +38,21 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        const user = users.getUser(socket.id);
+
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
+
         callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.lat, coords.lng));
+        const user = users.getUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.lat, coords.lng));
+        }
     });
 
     socket.on('disconnect', () => {
